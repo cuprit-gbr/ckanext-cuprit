a6b7e8ff8915173918c78697e7ca60efb07c3f2e
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2021-10-27 14:01:01 +0200

    reomove gaz_id

--- ckanext/cuprit/plugin.py
+++ ckanext/cuprit/plugin.py
@@ -39,42 +39,6 @@ class CupritPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm, DefaultT
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'cuprit')
 
-    # IDatasetForm
-    def create_package_schema(self):
-        schema = super(CupritPlugin, self).create_package_schema()
-        # our custom field
-        schema.update({
-            'gazetteer_id': [toolkit.get_validator('ignore_missing'),
-                            toolkit.get_converter('convert_to_extras')]
-        })
-        return schema
-
-    def update_package_schema(self):
-        schema = super(CupritPlugin, self).update_package_schema()
-        # our custom field
-        schema.update({
-            'gazetteer_id': [toolkit.get_validator('ignore_missing'),
-                            toolkit.get_converter('convert_to_extras')]
-        })
-        return schema
-
-    def show_package_schema(self):
-        schema = super(CupritPlugin, self).show_package_schema()
-        schema.update({
-            'gazetteer_id': [toolkit.get_converter('convert_from_extras'),
-                            toolkit.get_validator('ignore_missing')]
-        })
-        return schema
-
-    def is_fallback(self):
-        # Return True to register this plugin as the default handler for
-        # package types not handled by any other IDatasetForm plugin.
-        return True
-
-    def package_types(self):
-        # This plugin doesn't handle any special package types, it just
-        # registers itself as the default (above).
-        return []
 
     # IAuthFunctions
     def get_auth_functions(self):
@@ -110,3 +74,13 @@ class CupritPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm, DefaultT
         Called after a dataset has been created
         '''
         mailer.mail_dataset_created_to_admins(context, pkg_dict)
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
+
+    def package_types(self):
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
