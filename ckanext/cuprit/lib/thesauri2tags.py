31fd6b0aa17aee3e72910e03745b39e7386c8c2d
Toni <toni.schoenbuchner@csgis.de>
2023-01-11 09:41:42 +0100

    Simplify nested if statements

--- ckanext/cuprit/lib/thesauri2tags.py
+++ ckanext/cuprit/lib/thesauri2tags.py
@@ -2,7 +2,7 @@ import json
 import pickle
 
 # load data from thesauri.json file
-with open("thesauri.json", "r",  encoding="utf-8") as f:
+with open("tags_export.json", "r",  encoding="utf-8") as f:
     data = json.load(f)
 
 # keys defining relations
@@ -11,8 +11,31 @@ child_relations = "http://www.w3.org/2004/02/skos/core#narrower"
 top_level_parent_id = "http://thesauri.dainst.org/_fe65f286"
 names = "http://www.w3.org/2004/02/skos/core#prefLabel"
 exclude_parent_terms = ["Geopolitische Einheiten", "Natürliche Prozesse", "Raumbezogene Einheiten"]
-exclude_child_terms = ["Ereignisse", "Forschungspraktiken", "Fiktionale und übernatürliche Wesen", "Konzepte des menschlichen Zusammenlebens", "Konzepte in den Naturwissenschaften", "Theoretische Konzepte", "Sprachen", "Truppeneinheiten"]
+exclude_child_terms = ["Ereignisse", "Forschungspraktiken", "Fiktionale und übernatürliche Wesen",
+                       "Konzepte des menschlichen Zusammenlebens", "Konzepte in den Naturwissenschaften",
+                       "Theoretische Konzepte", "Sprachen", "Truppeneinheiten"]
 
+
+def find_parents(data: dict) -> list:
+    """
+    Find all parents of the given data.
+    """
+    flattend_data = {}
+    mapping_dict = {}
+    for element in data:
+        if parent_relation not in element:
+            continue
+        if element[parent_relation][0]["@id"] != top_level_parent_id:
+            continue
+        for e in element[names]:
+            if e["@language"] != "de":
+                continue
+            de_term = e["@value"]
+            element_id = element["@id"]
+            flattend_data[de_term] = []
+            mapping_dict[element_id] = de_term
+
+    return [flattend_data, mapping_dict]
 
 # Todo: maybe merge with find_relations() and use generator instead
 def find_parents(data: dict) -> list:
@@ -22,12 +45,14 @@ def find_parents(data: dict) -> list:
     flattened_data = {}
     mapping_dict = {}
     for element in data:
-        if parent_relation in element:
-            if element[parent_relation][0]["@id"] == top_level_parent_id:
-                for e in element[names]:
-                    if e["@language"] == "de" and e["@value"] not in exclude_parent_terms:
-                        flattened_data[e["@value"]] = []
-                        mapping_dict[element["@id"]] = e["@value"]
+        if parent_relation not in element:
+            continue
+        if element[parent_relation][0]["@id"] != top_level_parent_id:
+            continue
+        for e in element[names]:
+            if e["@language"] == "de" and e["@value"] not in exclude_parent_terms:
+                flattened_data[e["@value"]] = []
+                mapping_dict[element["@id"]] = e["@value"]
     return [flattened_data, mapping_dict]
 
 
@@ -36,16 +61,17 @@ def find_relations(parent_key: str, data: dict, childs: list) -> list:
     Find all partent child relations
     """
     for element in data:
-        if element["@id"] == parent_key:
-            has_relations = element.get(child_relations)
-            if has_relations is None:
-                for e in element[names]:
-                    if e["@language"] == "de" and e["@value"] not in exclude_child_terms:
-                        childs.append(e["@value"])
-                    continue
-            else:
-                for relation in has_relations:
-                    find_relations(relation["@id"], data, childs)
+        if element["@id"] != parent_key:
+            continue
+        has_relations = element.get(child_relations)
+        if has_relations is None:
+            for e in element[names]:
+                if e["@language"] == "de" and e["@value"] not in exclude_child_terms:
+                    childs.append(e["@value"])
+                continue
+        else:
+            for relation in has_relations:
+                find_relations(relation["@id"], data, childs)
 
     return childs
 
